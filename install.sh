ce5f68d2e3ab593716e4d913b1b8fd55b3ffa6c4
Aniverse <369842286@qq.com>
2020-03-17 14:07:47 +0800

    Hide outputs to log

--- install.sh
+++ install.sh
@@ -3,12 +3,30 @@
 # https://github.com/Aniverse/qbittorrent-nox-static
 # Author: Aniverse
 script_update=2020.03.17
-script_version=r10000
+script_version=r10001
+################################################################################################
+
+usage_guide() {
+s=$HOME/install.sh;rm -f $s;nano $s;chmod 755 $s
+bash $HOME/install.sh aniverse test123
+bash <(wget -qO- https://github.com/Aniverse/qbittorrent-nox-static/raw/master/install.sh)
+}
 ################################################################################################
 
 iUser=$1
 iPass=$2
 [[ -z $iUser ]] && echo -e "Work in progress, do not use this script for now ..." && exit
+serverip=$(wget --no-check-certificate -t1 -T6 -qO- v4.ipv6-test.com/api/myip.php)
+
+################################################################################################
+
+AppName=qBittorrent
+shared=1
+source <(wget -qO- https://github.com/Aniverse/inexistence/raw/master/00.Installation/function)
+set_variables_log_location
+
+################################################################################################
+
 
 if [[ $EUID != 0 ]]; then
     Root=0
@@ -20,21 +38,27 @@ else
 fi
 
 function install_qbittorrent_nox_static(){
-    wget https://sourceforge.net/projects/inexistence/files/qbittorrent/qbittorrent-nox.4.2.1.lt.1.1.14.strip/download -O ${BinPath}/qbittorrent-nox
-    chmod +x ${BinPath}/qbittorrent-nox
+    wget https://sourceforge.net/projects/inexistence/files/qbittorrent/qbittorrent-nox.4.2.1.lt.1.1.14/download -O ${BinPath}/qbittorrent-nox >> $OutputLOG 2>&1
+    chmod +x ${BinPath}/qbittorrent-nox >> $OutputLOG 2>&1
+    status_lock=$AppName
+    echo "status_lock=$status_lock" > $tmp_dir/Variables
+    rm -f $tmp_dir/$status_lock.1.lock $tmp_dir/$status_lock.2.lock 
+    [[ -x ${BinPath}/qbittorrent-nox]] && touch $tmp_dir/$status_lock.1.lock || $tmp_dir/$status_lock.2.lock
 }
 
 function configure_qbittorrent_nox(){
     if [[ $Root == 1 ]]; then
-        adduser --gecos "" $iUser --disabled-password --force-badname
-        echo "$iUser:$iPass" | chpasswd
-        bash <(wget -qO- https://github.com/Aniverse/inexistence/raw/master/00.Installation/package/qbittorrent/configure) -u $iUser -p $iPass
+        adduser --gecos "" $iUser --disabled-password --force-badname >> $OutputLOG 2>&1
+        echo "$iUser:$iPass" | chpasswd >> $OutputLOG 2>&1
+        bash <(wget -qO- https://github.com/Aniverse/inexistence/raw/master/00.Installation/package/qbittorrent/configure -o /dev/null) -u $iUser -p $iPass
+        echo -e "http://$serverip:2017"
     else
-        wget https://github.com/Aniverse/inexistence/raw/master/00.Installation/package/qbittorrent/configure -O ${BinPath}/qbconf
+        wget https://github.com/Aniverse/inexistence/raw/master/00.Installation/package/qbittorrent/configure -O ${BinPath}/qbconf >> $OutputLOG 2>&1
         chmod +x ${BinPath}/qbconf
     fi
 }
 
-
-install_qbittorrent_nox_static
+echo_task "Installing $AppName ..."
+install_qbittorrent_nox_static & spinner $!
+check_status $status_lock
 configure_qbittorrent_nox
