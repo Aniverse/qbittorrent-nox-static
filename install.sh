732c1f88fcadf7d93510453860551bd46092e95c
Aniverse <369842286@qq.com>
2020-03-17 14:28:13 +0800

    bug fix

--- install.sh
+++ install.sh
@@ -3,12 +3,13 @@
 # https://github.com/Aniverse/qbittorrent-nox-static
 # Author: Aniverse
 script_update=2020.03.17
-script_version=r10002
+script_version=r10003
 ################################################################################################
 
 usage_guide() {
 s=$HOME/install.sh;rm -f $s;nano $s;chmod 755 $s
-bash $HOME/install.sh aniverse test123
+bash $HOME/install.sh aaaa bbb 2021 9005
+
 bash <(wget -qO- https://github.com/Aniverse/qbittorrent-nox-static/raw/master/install.sh) aniverse test123
 }
 ################################################################################################
@@ -24,23 +25,21 @@ serverip=$(wget --no-check-certificate -t1 -T6 -qO- v4.ipv6-test.com/api/myip.ph
 
 ################################################################################################
 
-AppName=qBittorrent
-shared=1
-source <(wget -qO- https://github.com/Aniverse/inexistence/raw/master/00.Installation/function)
-set_variables_log_location
-
-################################################################################################
-
-
-
 if [[ $EUID != 0 ]]; then
     Root=0
     BinPath=$HOME/.local/bin
+    shared=1
     mkdir -p ${BinPath}
 else
     Root=1
     BinPath=/usr/bin
 fi
+
+source <(wget -qO- https://github.com/Aniverse/inexistence/raw/master/00.Installation/function)
+set_variables_log_location
+
+################################################################################################
+
 
 function install_qbittorrent_nox_static(){
     wget https://sourceforge.net/projects/inexistence/files/qbittorrent/qbittorrent-nox.4.2.1.lt.1.1.14/download -O ${BinPath}/qbittorrent-nox >> $OutputLOG 2>&1
@@ -56,7 +55,7 @@ function configure_qbittorrent_nox(){
         adduser --gecos "" $iUser --disabled-password --force-badname >> $OutputLOG 2>&1
         echo "$iUser:$iPass" | chpasswd >> $OutputLOG 2>&1
         bash <(wget -qO- https://github.com/Aniverse/inexistence/raw/master/00.Installation/package/qbittorrent/configure -o /dev/null) -u $iUser -p $iPass -w $webport -i $iport
-        echo -e "http://$serverip:$webport"
+        echo -e "\n${cyan}qBittorrent WebUI${normal} http://$serverip:$webport\n"
     else
         wget https://github.com/Aniverse/inexistence/raw/master/00.Installation/package/qbittorrent/configure -O ${BinPath}/qbconf >> $OutputLOG 2>&1
         chmod +x ${BinPath}/qbconf
@@ -64,14 +63,11 @@ function configure_qbittorrent_nox(){
 }
 
 
-
 ################################################################################################
 
 
-
-
 if [[ ! -x ${BinPath}/qbittorrent-nox ]] ; then
-    echo_task "Installing $AppName ..."
+    echo_task "Installing qBittorrent ..."
     install_qbittorrent_nox_static & spinner $!
     check_status $status_lock
 fi
